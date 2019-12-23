01b2583a90341572d3a630c8547df16476c40f77
Eric H <github.jtekt@gmail.com>
2019-12-23 13:03:32 -0700

    New Case Function: Update-LrCasePlaybookProcedure
    Update asignee, status, duedate, notes for a given procedure on a playbook that has been assigned to a case.

--- src/Public/LogRhythm/Case/Update-LrCasePlaybookProcedure.ps1
+++ src/Public/LogRhythm/Case/Update-LrCasePlaybookProcedure.ps1
@@ -21,9 +21,9 @@ Function Update-LrCasePlaybookProcedure {
     .PARAMETER PlaybookId
         Unique identifier for the playbook as an RFC 4122 formatted string, or as the playbook name.
     .PARAMETER ProcedureId
-        Unique identifier for the procedure, either as an RFC 4122 formatted string, or as the procedure name.
+        Unique identifier for the procedure, either as an RFC 4122 formatted string.
     .PARAMETER Assignee
-        Unique, numeric identifier for the person to which procedure is assigned.
+        Unique, numeric identifier, or user name, for the person to which procedure is assigned.
     .PARAMETER Notes
         Notes about the procedure.  <= 1000 characters
     .PARAMETER DueDate
@@ -34,15 +34,13 @@ Function Update-LrCasePlaybookProcedure {
         [System.Object]   ->  CaseId
         [System.String]   ->  PlaybookId
         [System.String]   ->  ProcedureId
-        [System.Integer]  ->  Assignee
+        [System.String]   ->  Assignee
         [System.String]   ->  Notes
         [System.String]   ->  DueDate
         [System.String]   ->  Status
     .OUTPUTS
         System.Object representing the returned LogRhythm playbook procedures on the applicable case.
 
-        If a match is not found, this cmdlet will throw exception
-        [System.Collections.Generic.KeyNotFoundException]
     .EXAMPLE
         PS C:\> Update-LrCasePlaybookProcedure -Credential $Token -CaseId "F47CF405-CAEC-44BB-9FDB-644C33D58F2A"
 
@@ -85,17 +83,17 @@ Function Update-LrCasePlaybookProcedure {
             Position = 4
         )]
         [ValidateNotNullOrEmpty()]
-        [integer] $Assignee,
+        [string] $Assignee,
 
         [Parameter(
-            Mandatory = $true,
+            Mandatory = $false,
             Position = 5
         )]
         [ValidateNotNullOrEmpty()]
         [string] $Notes,
 
         [Parameter(
-            Mandatory = $true,
+            Mandatory = $false,
             Position = 6
         )]
         [ValidateNotNullOrEmpty()]
@@ -127,15 +125,66 @@ Function Update-LrCasePlaybookProcedure {
             throw [ArgumentException] "Parameter [CaseId] should be an RFC 4122 formatted string or an integer."
         }
 
+        # Get Case Guid
+        $CaseGuid = (Get-LrCaseById -Id $CaseId).id
+
         # Validate or Retrieve Playbook Id
+
+        # Populate list of Case Playbooks
+        $CasePlaybooks = Get-LrCasePlaybooks -Id $CaseId
+        
         if ($PlaybookId) {
-            # Validate Playbook Id
-            if (! (Test-Guid $PlaybookId)) {
-                throw [ArgumentException] "Parameter [PlaybookId] should be an RFC 4122 formatted string."
+            if ($CasePlaybooks -eq $null) {
+                throw [ArgumentException] "No Playbooks located on case: $CaseId."
+            } else {
+                # Validate Playbook Id
+                $PlaybookType = Test-LrPlaybookIdFormat -Id $PlaybookId
+                # Step through array of Playbooks assigned to case looking for match
+                if ($CasePlaybooks -is [array]) {
+                    $CasePlaybooks | ForEach-Object {
+                        Write-Verbose "[$Me]: $($_.Name) compared to $($PlaybookId)"
+                        if ($PlaybookType.isguid -eq $false) {
+                            if($($_.Name).ToLower() -eq $($PlaybookId).ToLower()) {
+                                Write-Verbose "[$Me]: Matched Playbook Name: $PlaybookId To PlaybookId: $($_.Id)"
+                                $PlaybookGuid = $_.Id
+                            } 
+                        } elseif ($PlaybookType.isguid -eq $true) {
+                            if($($_.Id).ToLower() -eq $($PlaybookId).ToLower()) {
+                                Write-Verbose "[$Me]: Matched Playbook Name: $PlaybookId To PlaybookId: $($_.Id)"
+                                $PlaybookGuid = $_.Id
+                            }
+                        }
+                    } 
+                    if ($PlaybookGuid -eq $null) {
+                        throw [ArgumentException] "Parameter [PlayBookId:$PlaybookId] cannot be matched to playbooks on case: $CaseId."
+                    }
+                } else {
+                    # Step through single Playbook assigned to case looking for match
+                    if ($PlaybookType.isguid -eq $false) {
+                        if($($CasePlaybooks.Name).ToLower() -eq $($PlaybookId).ToLower()) {
+                            Write-Verbose "[$Me]: Matched Playbook Name: $PlaybookId To PlaybookId: $($CasePlaybooks.Id)"
+                            $PlaybookGuid = $CasePlaybooks.Id
+                        } else {
+                            throw [ArgumentException] "Parameter [PlayBookId:$PlaybookId] cannot be located on case: $CaseId."
+                        }
+                    } elseif ($PlaybookType.isguid -eq $true) {
+                        if($($CasePlaybooks.Id).ToLower() -eq $($PlaybookId).ToLower()) {
+                            Write-Verbose "[$Me]: Matched Playbook Name: $PlaybookId To PlaybookId: $($CasePlaybooks.Id)"
+                            $PlaybookGuid = $CasePlaybooks.Id
+                        } else {
+                            throw [ArgumentException] "Parameter [PlayBookId:$PlaybookId] cannot be located on case: $CaseId."
+                        }
+                    }
+                }
             }
         } else {
-            #This will require verification once the function below is validated.
-            $PlaybookId = (Get-LrCasePlaybooks -Id $IdInfo)[0].Pid
+            # No matches.  Only one playbook assigned to case.  Default to single Playbook assigned to case
+            if (($CasePlaybooks).Count -ge 2) {
+                throw [ArgumentException] "No Playbook specified.  More than one playbook assigned to case: $CaseId."
+            } elseif ($CasePlaybooks) {
+                $PlaybookGuid = $CasePlaybooks.Id
+                Write-Verbose "[$Me]: No Playbook specified.  One Playbook on case, applying PlaybookId: $PlaybookId"
+            }
         }
 
         # Design note
@@ -147,19 +196,96 @@ Function Update-LrCasePlaybookProcedure {
         # Request Headers
         $Headers = [Dictionary[string,string]]::new()
         $Headers.Add("Authorization", "Bearer $Token")
-        
-
+    
         # Request URI
         $Method = $HttpMethod.Put
-        $RequestUri = $BaseUrl + "/cases/$IdInfo/playbooks/$PlaybookId/procedures/$ProcedureId/"
+        $RequestUri = $BaseUrl + "/cases/$CaseGuid/playbooks/$PlaybookGuid/procedures/$ProcedureId/"
         Write-Verbose "[$Me]: RequestUri: $RequestUri"
 
+        # Inspect Note for Procedure Limitation
+        if ($Notes) {
+            if ($Notes.Length -gt 1000) {
+                throw [ArgumentException] "Parameter [Notes] exceeded length limit.  1000:$($Notes.Length)"
+            }
+        }
+
+        # Validate Status is proper
+        if ($Status) {
+            $ValidStatus = @("notcompleted", "completed", "skipped")
+            
+            if ($ValidStatus.Contains($Status.ToLower())) {
+                Switch ($Status.ToLower())
+                {
+                 notcompleted { $Status = "NotCompleted" }
+                 completed { $Status = "Completed" }
+                 skipped { $Status = "Skipped" }
+                }
+            } else {
+                throw [ArgumentException] "Parameter [Status] should be: NotCompleted, Completed, or Skipped."
+            }
+        }
+
+        # Validate Assignee is valid
+        if ($Assignee) {
+            $AssigneeType = Test-LrUserIdFormat -Id $Assignee
+            if ($AssigneeType.IsInt -eq $false) {
+                $AssigneeResult = Get-LrUsers -Name $Assignee
+                Write-Verbose "[$Me]: Assignee String: $Assignee Assignee Result: $($AssigneeResult.Name)"
+                if ($AssigneeResult) {
+                    if ($AssigneeResult.disabled -eq $true) {
+                        throw [ArgumentException] "Parameter [Assignee:$Assignee] is currently disabled"
+                    } else {
+                        [int32] $AssigneeNumber = $AssigneeResult.number
+                    }
+                } else {
+                    throw [ArgumentException] "Parameter [Assignee:$Assignee] not found in LrUsers"
+                }
+            } elseif ($AssigneeType.IsInt -eq $true) {
+                $AssigneeResult = Get-LrUsers | Select-Object number, disabled | Where-Object number -eq $Assignee
+                Write-Verbose "[$Me]: Assignee Int: $Assignee Assignee Result: $($AssigneeResult.Name)"
+                if ($AssigneeResult) {
+                    if ($AssigneeResult.disabled -eq $true) {
+                        throw [ArgumentException] "Parameter [Assignee:$Assignee] is currently disabled"
+                    } else {
+                        [int32] $AssigneeNumber = $AssigneeResult.number
+                    }
+                } else {
+                    throw [ArgumentException] "Parameter [Assignee:$Assignee] not found in LrUsers"
+                }
+            } else {
+                throw [ArgumentException] "Parameter [Assignee] must be valid user name or user id #"
+            }
+
+            $CaseCollaborators = Get-LrCaseById -Id $CaseId | Select-Object collaborators -ExpandProperty collaborators
+            if (!$CaseCollaborators.number.Contains($AssigneeNumber)) {
+                throw [ArgumentException] "Parameter [Assignee:$Assignee] not a collaborator on case $CaseId"
+            }
+        }
+
+        # Request Body
+        $Body = [PSObject]@{}
+        if ($Assignee) {
+            $Body | Add-Member -NotePropertyName assignee -NotePropertyValue $AssigneeNumber
+        }
+        if ($notes) {
+            $Body | Add-Member -NotePropertyName notes -NotePropertyValue $Notes
+        }
+        if ($DueDate) {
+            $Body | Add-Member -NotePropertyName dueDate -NotePropertyValue $Notes
+        }
+        if ($Status) {
+            $Body | Add-Member -NotePropertyName status -NotePropertyValue $Status
+        }
+        $Body = $Body | ConvertTo-Json
+        
         # REQUEST
+        Write-Verbose "[$Me]: request body is:`n$Body"
         try {
             $Response = Invoke-RestMethod `
                 -Uri $RequestUri `
                 -Headers $Headers `
                 -Method $Method `
+                -Body $Body`
         }
         catch [System.Net.WebException] {
             $Err = Get-RestErrorMessage $_
