41b648322d683d366954255a26c1dea1a921a97e
Gene Cupstid <gene.cupstid@chrobinson.com>
2020-01-25 19:59:21 -0600

    Update Program.cs

--- packages/SrfSetup/Program.cs
+++ packages/SrfSetup/Program.cs
@@ -11,6 +11,7 @@ namespace SrfSetup
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// test
         [STAThread]
         static void Main()
         {
