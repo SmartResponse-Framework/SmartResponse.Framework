d56f029dfc33d1e67948a1881b19ad2dd32cdf79
Gene Cupstid <gene.cupstid@chrobinson.com>
2020-05-05 19:56:29 -0500

    work on installer
    Added Get-LrCaseMetrics - from upstream
    Added New-LrPsConfig - checks / creates config items
    Removed Test-ConfigExists - not needed

--- src/SmartResponse.Framework.psm1
+++ src/SmartResponse.Framework.psm1
@@ -1,8 +1,9 @@
 
 #region: Module Info                                                                     
 # Module Name: To make it easier to change the name of the module.
-# NOTE: There is a corresponding set of variables in the build module's psm1.  If these
-#       are changed, then the variables there should be updated to the same.
+# NOTE: These two variables should be set exactly the same as they appear in setup\New-LrPsConfig!
+#       The name of the file may be ModuleName.preferences.json, but the object is still called
+#       [SrfPreferences] - too many things reference that now to be changed without extra testing.
 $ModuleName = "LrPs"
 $PreferencesFileName = $ModuleName + ".preferences.json"
 
@@ -19,23 +20,25 @@ $AssemblyList = [PSCustomObject]@{
 }
 
 # Includes Dir
-$SrfIncludes = [System.IO.DirectoryInfo]::new((Join-Path -Path $PSScriptRoot -ChildPath "Include"))
+$IncludeDirPath = [System.IO.DirectoryInfo]::new((Join-Path -Path $PSScriptRoot -ChildPath "Include"))
 #endregion
 
 
 
 #region: Load Preferences                                                                
-$PrefPath = Join-Path `
+$ConfigDirPath = Join-Path `
     -Path ([Environment]::GetFolderPath("LocalApplicationData"))`
-    -ChildPath $ModuleName | Join-Path -ChildPath $PreferencesFileName
-$PrefInfo = [System.IO.FileInfo]::new($PrefPath)
+    -ChildPath $ModuleName
 
-if ($PrefInfo.Exists) {
-    $SrfPreferences = Get-Content -Path $PrefInfo.FullName -Raw | ConvertFrom-Json
+$ConfigFileInfo = [System.IO.FileInfo]::new((Join-Path -Path $ConfigDirPath -ChildPath $PreferencesFileName))
+
+# Try to load the Config File from Local AppData, fallback to the copy in the install directory.
+if ($ConfigFileInfo.Exists) {
+    $SrfPreferences = Get-Content -Path $ConfigFileInfo.FullName -Raw | ConvertFrom-Json
 } else {
     Write-Host "Warning: Unable to locate preferences directory - module will load copy from installation/include directory."
     $SrfPreferences = Get-Content -Path `
-        (Join-Path -Path $SrfIncludes.FullName -ChildPath $PreferencesFileName) -Raw | ConvertFrom-Json
+        (Join-Path -Path $IncludeDirPath.FullName -ChildPath $PreferencesFileName) -Raw | ConvertFrom-Json
 }
 #endregion
 
@@ -103,23 +106,22 @@ foreach ($include in $Includes.GetEnumerator()) {
 #region: Import API Keys                                                                 
 # LogRhythm API Key
 
-$KeyPath = $SrfPreferences.LrDeployment.LrApiCredentialPath
+$KeyPath = Join-Path -Path $ConfigDirPath -ChildPath "LrApiToken.xml"
 
 try {
     $SrfPreferences.LrDeployment.LrApiCredential = Import-Clixml -Path $KeyPath
-    Write-Host "  > LogRhythm API Key Loaded." -ForegroundColor Green
 }
 catch [System.IO.FileNotFoundException] {
-    Write-Host "Warning: LogRhythm API Credential not found." -ForegroundColor Yellow
+    Write-Host "Warning: LrApiToken.xml not found in $ConfigDirPath" -ForegroundColor Yellow
     Write-Host "LogRhythm cmdlets will need to specify the '-Credential' option in order to function." `
     -ForegroundColor Yellow
 }
 catch [System.Security.Cryptography.CryptographicException] {
-    Write-Host "Unable to load key, insufficient permissions. Run Setup script." `
+    Write-Host "Unable to load key, insufficient permissions. Run the Setup script again to create a new credential file." `
     -ForegroundColor Yellow
 }
 catch [System.Exception] {
-    Write-Host "Unexpected error while attempting to load LogRhythm API Credential."
+    Write-Host "Unexpected error while attempting to load LogRhythm API Credential." -ForegroundColor Yellow
     Write-Host "LogRhythm cmdlets will need to specify the '-Credential' option in order to function." `
     -ForegroundColor Yellow
 }
